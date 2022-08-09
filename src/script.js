f30b23628be1bde0298e7a7d973c1c05871c21da
Brett Jones <677025+BrettJay@users.noreply.github.com>
2022-08-09 11:33:15 +0800

    feat: Adjustable-width canvas element
    Three.js determines the aspect ratio of the canvas element differently
    depending on the camera in use.
    
    This spline scene uses the [THREE.OrthographicCamera][1], which uses
    left, right, top, and bottom arguments to determine the aspect ratio
    rendered inside the 3d scene rendered to the canvas element.
    
    By default, Spline assumes the viewport of the canvas element should be
    the full browser width and height, so if the canvas element is styled to
    be a different aspect ratio than the intrinsic viewport with CSS, the
    scene rendered inside the canvas element will appear distorted.
    
    This demo uses [lil-gui][2] to provide a debug interface to a `width`
    ratio variable, which we can adjust between 0–1 to represent the
    proportional width of the browser the canvas should occupy. This
    variable is referenced when the three js orthographic camera is
    instantiated on initial render, and whenever the browser is resized.
    
    This allows us to avoid the canvas being squashed by CSS and
    providing a squashed looking render.
    
    [1]: https://threejs.org/docs/#api/en/cameras/OrthographicCamera
    [2]: https://github.com/georgealways/lil-gui

--- src/script.js
+++ src/script.js
@@ -1,9 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import SplineLoader from '@splinetool/loader';
+import * as dat from 'lil-gui';
+
+const element = {
+  width: 0.5,
+}
 
 // camera
-const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2,  -100000, 100000);
+const camera = new THREE.OrthographicCamera(window.innerWidth / - ( 2 / element.width ), window.innerWidth / ( 2 / element.width ), window.innerHeight / 2, window.innerHeight / - 2,  -100000, 100000);
 camera.position.set(52.39, 1000.55, 227.45);
 camera.quaternion.setFromEuler(new THREE.Euler(-1.47, 0.01, 0.06));
 
@@ -21,7 +26,7 @@ loader.load(
 
 // renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth * element.width, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
@@ -38,16 +43,22 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.125;
 
 window.addEventListener('resize', onWindowResize);
+
 function onWindowResize() {
-  camera.left = window.innerWidth / - 2;
-  camera.right = window.innerWidth / 2;
+  camera.left = window.innerWidth / - ( 2 / element.width );
+  camera.right = window.innerWidth / ( 2 / element.width );
   camera.top = window.innerHeight / 2;
   camera.bottom = window.innerHeight / - 2;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth * element.width, window.innerHeight);
 }
 
 function animate(time) {
   controls.update();
   renderer.render(scene, camera);
 }
+
+// Debug UI
+const gui = new dat.GUI();
+
+gui.add( element, 'width', 0.1, 1).onChange( onWindowResize ).name( 'Width (ratio)' )
